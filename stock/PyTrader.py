a67ed8aecec51f684f7d760fbeb7eb7e5acc4c48
Yutae <cyt9772@naver.com>
2023-01-02 13:16:01 +0900

    end of 2nd day

--- stock/PyTrader.py
+++ stock/PyTrader.py
@@ -12,6 +12,7 @@ class MyWindow(QMainWindow, form_class):
         self.setupUi(self)
 
         self.lineEdit.textChanged.connect(self.code_changed)
+        self.pushButton.clicked.connect(self.send_order)
 
         self.kiwoom=Kiwoom()
         self.kiwoom.comm_connect()
@@ -19,6 +20,12 @@ class MyWindow(QMainWindow, form_class):
         self.timer = QTimer(self)
         self.timer.start(1000)
         self.timer.timeout.connect(self.timeout)
+
+        #get account information
+        accouns_num=int(self.kiwoom.get_login_info("ACCOUNT_CNT"))
+        accounts=self.kiwoom.get_login_info("ACCNO")
+        accounts_list=accounts.split(';')[0:accouns_num]
+        self.comboBox.addItems(accounts_list)
 
     def timeout(self):
         current_time=QTime.currentTime()
@@ -37,6 +44,20 @@ class MyWindow(QMainWindow, form_class):
         code=self.lineEdit.text()
         name=self.kiwoom.get_master_code_name(code)
         self.lineEdit_2.setText(name)
+
+    def send_order(self):
+        order_type_lookup={'신규매수':1, '신규매도':2, '매수취소':3, '매도취소':4}
+        hoga_lookup={'지정가':"00",'시장가':"03"}
+
+        account=self.comboBox.currentText()
+        order_type=self.comboBox_2.currentText()
+        code=self.lineEdit.text()
+        hoga=self.comboBox_3.currentText()
+        num=self.spinBox.value()
+        price=self.spinBox_2.value()
+
+        self.kiwoom.send_order("send_order_req","0101",account, order_type_lookup[order_type],code, num, price, hoga_lookup[hoga],"")
+
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
